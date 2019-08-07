c4d3558ddc773dbec831517295ece5c3236c7c8f
Martin Zugnoni <martin.zugnoni@gmail.com>
2019-08-07 18:11:47 -0400

    Fix failing tests

--- unit-14-exceptions/lesson-6-reimplement-pop/tests/test_invalid_key_default.py
+++ unit-14-exceptions/lesson-6-reimplement-pop/tests/test_invalid_key_default.py
@@ -1,10 +1,9 @@
-test_dict = {
-    "cat": "hat",
-    "dog": "log",
-    "elf": "shelf"
-}
-
-
 def test_invalid_key_default():
+    test_dict = {
+        "cat": "hat",
+        "dog": "log",
+        "elf": "shelf"
+    }
+
     assert pop(test_dict, "horse", "crickets") == "crickets"
     assert len(test_dict) == 3
--- unit-14-exceptions/lesson-6-reimplement-pop/tests/test_invalid_key_no_default.py
+++ unit-14-exceptions/lesson-6-reimplement-pop/tests/test_invalid_key_no_default.py
@@ -1,13 +1,12 @@
 import pytest
 
 
-test_dict = {
-    "cat": "hat",
-    "dog": "log",
-    "elf": "shelf"
-}
-
-
 def test_invalid_key_no_default():
+    test_dict = {
+        "cat": "hat",
+        "dog": "log",
+        "elf": "shelf"
+    }
+
     with pytest.raises(KeyError):
         pop(test_dict, "horse")
